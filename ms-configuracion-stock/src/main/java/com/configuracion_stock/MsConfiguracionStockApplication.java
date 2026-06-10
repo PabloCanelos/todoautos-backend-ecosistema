6dd7ddf45b2d01b56a4fe33d33ced3a3bf567829
Pablo Andres Canelos Canelos <pabloccanelos.contacto@gmail.com>
2026-06-10 14:51:13 -0400

    feat:se agregan ms a eureka server

--- ms-configuracion-stock/src/main/java/com/configuracion_stock/MsConfiguracionStockApplication.java
+++ ms-configuracion-stock/src/main/java/com/configuracion_stock/MsConfiguracionStockApplication.java
@@ -1,8 +1,8 @@
 package com.configuracion_stock;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsConfiguracionStockApplication {
 
--- ms-usuarios-accesos/src/main/java/com/todoautos/usuarios/MsUsuariosAccesosApplication.java
+++ ms-usuarios-accesos/src/main/java/com/todoautos/usuarios/MsUsuariosAccesosApplication.java
@@ -1,8 +1,8 @@
 package com.todoautos.usuarios;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsUsuariosAccesosApplication {
 
