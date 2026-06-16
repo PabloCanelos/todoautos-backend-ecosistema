9903590da3264c7f677e16b4ffbde182cfe9974e
Pablo Andres Canelos Canelos <pabloccanelos.contacto@gmail.com>
2026-06-16 14:44:30 -0400

    configuracion  pom xml gateway, integracion con MS

---
+++ api-gateway/src/main/java/com/todoautos/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.todoautos.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
--- ms-usuarios-accesos/src/main/java/com/todoautos/usuarios/MsUsuariosAccesosApplication.java
+++ ms-usuarios-accesos/src/main/java/com/todoautos/usuarios/MsUsuariosAccesosApplication.java
@@ -2,6 +2,7 @@ package com.todoautos.usuarios;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MsUsuariosAccesosApplication {
